b79c292bf46f88609d12892403e48c95ed3a550b
Michail <vsivbi@mail.ru>
2024-04-09 17:09:44 +0500

    Добавил возможность быстрого удаления данных из БД через if/else и добавил добавление MB из txt файла

--- base_filling.py
+++ base_filling.py
@@ -20,26 +20,47 @@ GPU_MBs = session.query(GPU_MB).all()
 RAM_MBs = session.query(RAM_MB).all()
 Cooler_MBs = session.query(Cooler_MB).all()
 
-# Открываем и считываем
-with open("Base_filling_txts/CPU.txt", "r") as f:
-    cpus = f.read()
-cpus = cpus.replace(" $ ", "\n")# Заменяем все символы $ на пробелы
-cpus = cpus.split("\n")# Разбиваем текст на список строк
-i = 0
-while i in range(len(cpus)):
-    new_CPU = CPU(CPU_name=cpus[i], ALU=cpus[i+1], freq=cpus[i+2], socket=cpus[i+3], TDP=cpus[i+4])
-    session.add(new_CPU)
-    i += 5
+
+del_or_cre = 1
+if del_or_cre == 1:
+    # Работа с CPU
+    with open("Base_filling_txt/CPU.txt", "r") as f:# Читаем CPU
+        cpus = f.read()
+    cpus = cpus.replace(" $ ", "\n")# Заменяем все символы $ на пробелы
+    cpus = cpus.split("\n")# Разбиваем текст на список строк
+    i = 0
+    while i in range(len(cpus)):
+        new_CPU = CPU(CPU_name=cpus[i], ALU=cpus[i+1], freq=cpus[i+2], socket=cpus[i+3], TDP=cpus[i+4])
+        session.add(new_CPU)
+        i += 5
+    # Работа с GPU
+    with open("Base_filling_txt/GPU.txt", "r") as f:# Открываем и считываем GPU
+        gpus = f.read()
+    gpus = gpus.replace(" $ ", "\n")# Заменяем все символы $ на пробелы
+    gpus = gpus.split("\n")# Разбиваем текст на список строк
+    i = 0
+    while i in range(len(gpus)):
+        new_GPU = GPU(GPU_name=gpus[i], freq=gpus[i+1], ALU=gpus[i+2], volume=gpus[i+3], GPU_type=gpus[i+4])
+        session.add(new_GPU)
+        i += 5
+    # Работа с MB
+    with open("Base_filling_txt/MB.txt", "r") as f:  # Открываем и считываем MB
+        mbs = f.read()
+    mbs = mbs.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
+    mbs = mbs.split("\n")  # Разбиваем текст на список строк
+    i = 0
+    while i in range(len(mbs)):
+        new_MB = MB(MB_name=mbs[i], form_factor=mbs[i + 1], socket_type=mbs[i + 2], RAM_type=mbs[i + 3], RAM_count=mbs[i + 4], freq=mbs[i + 5], GPU_type=mbs[i + 6])
+        session.add(new_MB)
+        i += 7
+    print('Create')
+else:
+    for cpus in CPUs:
+        session.delete(cpus)
+    for gpus in GPUs:
+        session.delete(gpus)
+    for mbs in MBs:
+        session.delete(mbs)
+    print('Delete')
 session.commit()
-with open("Base_filling_txts/GPU.txt", "r") as f:
-    gpus = f.read()
-gpus = gpus.replace(" $ ", "\n")# Заменяем все символы $ на пробелы
-gpus = gpus.split("\n")# Разбиваем текст на список строк
-i = 0
-while i in range(len(gpus)):
-    new_GPU = GPU(GPU_name=gpus[i], freq=gpus[i+1], ALU=gpus[i+2], volume=gpus[i+3], GPU_type=gpus[i+4])
-    session.add(new_GPU)
-    i += 5
-session.commit()
-print('Create')
 session.close()
