56f1d4cbbebb43bde7e9039ce2c5c2f3c4458ac4
Michail <vsivbi@mail.ru>
2024-04-12 21:42:26 +0500

    Можно выбрать процессор или материнку и выведится название оных

--- windows/add_lot_form.py
+++ windows/add_lot_form.py
@@ -1,20 +1,11 @@
 from typing import Iterable, Callable
 
-from PyQt5.QtWidgets import QWidget, QDialog
+from PyQt5.QtWidgets import QWidget, QDialog, QTableWidget, QTableWidgetItem
 
-from ORM import get_session, Lot, User
+from ORM import get_session, Lot, User, CPU, MB, GPU, Cooler, RAM, Memory, PU
 from ui_qt import UiAddLotForm
 #from .dialog import Dialog
 class LotAdd(QWidget, UiAddLotForm):
-    def __init__(self, callbacks: Iterable[Callable]):
-        super().__init__()
-        self.callbacks = callbacks
-        self.setupUi(self)
-        self.session = get_session()
-        self.push_button_create.clicked.connect(self.create_lot) #Создание окна
-        # self.comboBox.clicked.connect(self.create_lot)  # Создание окна
-        self.push_button_close.clicked.connect(lambda: self.close()) #Закрытие окна
-        self.comboBox.removeItem()
 
     def __init__(self, callbacks: Iterable[Callable]):
         super().__init__()
@@ -23,21 +14,34 @@ class LotAdd(QWidget, UiAddLotForm):
         self.session = get_session()
         self.push_button_create.clicked.connect(self.create_lot)  # Создание окна
         self.push_button_close.clicked.connect(lambda: self.close()) #Закрытие окна
-
         self.comboBox.activated.connect(self.activated) # Вызов функции activated
 
-    def activated(Self, index):
-        print("Activated index:", index)
-
-    def check_index(self, index):
-        CurIndex = self.combobox.currentIndex()
-        print(f"Index signal: {index}, currentIndex {CurIndex}")
+    def activated(self, index):
+        self.tableWidget.setRowCount(0)
+        if index == 1:
+            cpu = self.session.query(CPU).all()
+            for i in cpu:
+                rowPosition = self.tableWidget.rowCount()
+                self.tableWidget.insertRow(rowPosition)  # Создание строчки
+                self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(i.CPU_name))  # Заполняем строки
+                self.tableWidget.setItem(rowPosition, 1, QTableWidgetItem("Hello"))
+        elif index == 2:
+            mb = self.session.query(MB).all()
+            for i in mb:
+                rowPosition = self.tableWidget.rowCount()
+                self.tableWidget.insertRow(rowPosition)  # Создание строчки
+                self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(i.MB_name))  # Заполняем строки
+                self.tableWidget.setItem(rowPosition, 1, QTableWidgetItem("Hello"))
+        self.tableWidget.cellDoubleClicked.connect(self.create_lot)
 
     def create_lot(self):
+
+
         self.session.add()
         self.session.commit()
 
         self.custom_close()
+
 
     def custom_close(self):
         for callback in self.callbacks:
