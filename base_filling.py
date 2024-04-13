5556d63e723b513d5036f998cc56f95dac6f8ae8
Michail <vsivbi@mail.ru>
2024-04-13 19:15:29 +0500

    Теперь добавлен хард, проверка и запись совместимостей, изменено удаление инфы о всем в БД.

--- base_filling.py
+++ base_filling.py
@@ -7,23 +7,23 @@ GPUs = session.query(GPU).all()
 MBs = session.query(MB).all()
 PUs = session.query(PU).all()
 RAMs = session.query(RAM).all()
-Lots = session.query(Lot).all()
 Memorys = session.query(Memory).all()
 Coolers = session.query(Cooler).all()
-Buyers = session.query(Buyer).all()
-Sellers = session.query(Seller).all()
-Receipts = session.query(Receipt).all()
+
+Lots = session.query(Lot).all()
 Users = session.query(User).all()
+Sellers = session.query(Seller).all()
+Buyers = session.query(Buyer).all()
+Receipts = session.query(Receipt).all()
 
 CPU_MBs = session.query(CPU_MB).all()
 GPU_MBs = session.query(GPU_MB).all()
 RAM_MBs = session.query(RAM_MB).all()
 Cooler_MBs = session.query(Cooler_MB).all()
-
-
-del_or_cre = 1
+# 1-заполнение БД, 2-проверка совместимости, 3-удаление всей инфы из БД
+del_or_cre = 2
 if del_or_cre == 1:
-    # Работа с CPU
+    # Заполняем CPU
     with open("Base_filling_txt/CPU.txt", "r") as f:  # Читаем CPU
         new = f.read()
     new = new.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
@@ -34,7 +34,7 @@ if del_or_cre == 1:
         session.add(new_CPU)
         i += 5
     del new[:]
-    # Работа с GPU
+    # Заполняем GPU
     with open("Base_filling_txt/GPU.txt", "r") as f:  # Открываем и считываем GPU
         new = f.read()
     new = new.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
@@ -45,7 +45,7 @@ if del_or_cre == 1:
         session.add(new_GPU)
         i += 5
     del new[:]
-    # Работа с MB
+    # Заполняем MB
     with open("Base_filling_txt/MB.txt", "r") as f:  # Открываем и считываем MB
         new = f.read()
     new = new.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
@@ -56,7 +56,7 @@ if del_or_cre == 1:
         session.add(new_MB)
         i += 7
     del new[:]
-    # Работа с RAM
+    # Заполняем RAM
     with open("Base_filling_txt/RAM.txt", "r") as f:  # Открываем и считываем RAM
         new = f.read()
     new = new.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
@@ -67,7 +67,7 @@ if del_or_cre == 1:
         session.add(new_RAM)
         i += 4
     del new[:]
-    # Работа с PU
+    # Заполняем PU
     with open("Base_filling_txt/PU.txt", "r") as f:  # Открываем и считываем PU
         new = f.read()
     new = new.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
@@ -78,7 +78,7 @@ if del_or_cre == 1:
         session.add(new_pus)
         i += 2
     del new[:]
-    # Работа с Cooler
+    # Заполняем Cooler
     with open("Base_filling_txt/Cooler.txt", "r") as f:  # Открываем и считываем Cooler
         new = f.read()
     new = new.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
@@ -88,8 +88,81 @@ if del_or_cre == 1:
         new_pus = Cooler(cooler_name=new[i], socket=new[i + 1], DH=new[i + 2], noise=new[i + 3])
         session.add(new_pus)
         i += 4
+    # Заполняем Memory
+    with open("Base_filling_txt/Memory.txt", "r") as f:  # Открываем и считываем Cooler
+        new = f.read()
+    new = new.replace(" $ ", "\n")  # Заменяем все символы $ на пробелы
+    new = new.split("\n")  # Разбиваем текст на список строк
+    i = 0
+    while i in range(len(new)):
+        new_pus = Memory(mem_name=new[i], mem_type=new[i + 1], volume=new[i + 2], speed=new[i + 3])
+        session.add(new_pus)
+        i += 4
     print('Create')
+elif del_or_cre == 2:
+    # Делаем совместимость по сокету MotherBoard и CPU
+    i = 0
+    while i in range(len(CPUs)):
+        j = 0
+        while j in range(len(MBs)):
+            if CPUs[i].socket == MBs[j].socket_type:
+                new_MB_CPU = CPU_MB(CPU_id=CPUs[i].CPU_id, MB_id=MBs[j].MB_id)
+                session.add(new_MB_CPU)
+            j += 1
+        i += 1
+    # Делаем совместимость по сокету MotherBoard и GPU
+    i = 0
+    while i in range(len(GPUs)):
+        j = 0
+        while j in range(len(MBs)):
+            if GPUs[i].GPU_type == MBs[j].GPU_type:
+                new_MB_GPU = GPU_MB(GPU_id=GPUs[i].GPU_id, MB_id=MBs[j].MB_id)
+                session.add(new_MB_GPU)
+            j += 1
+        i += 1
+    i = 0
+    # Делаем совместимость по MotherBoard и RAM
+    while i in range(len(RAMs)):
+        j = 0
+        while j in range(len(MBs)):
+            if RAMs[i].RAM_type == MBs[j].RAM_type:
+                new_MB_RAM = RAM_MB(RAM_id=RAMs[i].RAM_id, MB_id=MBs[j].MB_id)
+                session.add(new_MB_RAM)
+            j += 1
+        i += 1
+    i = 0
+    # Делаем совместимость по MotherBoard и Cooler
+    while i in range(len(Coolers)):
+        j = 0
+        while j in range(len(MBs)):
+            if Coolers[i].socket == MBs[j].socket_type:
+                new_Cooler_CPU = Cooler_MB(cooler_id=Coolers[i].cooler_id, MB_id=MBs[j].MB_id)
+                session.add(new_Cooler_CPU)
+            j += 1
+        i += 1
+    print("Create compability")
 else:
+    # Удаление пользователей, чеков, лотов (вроде в правильном порядке)
+    for receipts in Receipts:
+        session.delete(receipts)
+    for buyers in Buyers:
+        session.delete(buyers)
+    for lots in Lots:
+        session.delete(lots)
+    for sellers in Sellers:
+        session.delete(sellers)
+    for users in Users:
+        session.delete(users)
+    # Удаление информации о совместимостей
+    for cpu_mb in CPU_MBs:
+        session.delete(cpu_mb)
+    for gpu_mb in GPU_MBs:
+        session.delete(gpu_mb)
+    for ram_mb in RAM_MBs:
+        session.delete(ram_mb)
+    for cooler_mb in Cooler_MBs:
+        session.delete(cooler_mb)
+    #Удаление информации о компонентах
     for cpus in CPUs:
         session.delete(cpus)
     for gpus in GPUs:
